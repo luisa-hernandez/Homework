7077aadf3692949c6676182513ba3de688566a2f
Hobo Noir <hobonetics@gmail.com>
2017-03-05 22:18:32 -0500

    implemented recursive bubble sort

--- Homework2/Sorts.java
+++ Homework2/Sorts.java
@@ -23,6 +23,48 @@ public class Sorts {
 		return ints;
 	}
 
+	public int[] recursiveBubble(int[] ints) {
+
+		// base case
+		if (ints.length == 1) {
+			return ints;
+		}
+
+		// one pass of normal bubble sort
+		for (int i = 0; i < ints.length - 1; i++) {
+			if (ints[i] > ints[i + 1]) {
+				int temp = ints[i];
+				ints[i] = ints[i + 1];
+				ints[i + 1] = temp;
+			}
+		}
+
+		// create subarray
+		int[] subArray = new int[ints.length - 1];
+		for (int i = 0; i <= subArray.length - 1; i++) {
+			subArray[i] = ints[i];
+		}
+
+		// recursion on 1st - 2nd to last element
+		subArray = recursiveBubble(subArray);
+
+		// put elements of sub array into array
+		for (int i = 0; i <= subArray.length - 1; i++) {
+			ints[i] = subArray[i];
+		}
+
+		// one last pass of normal bubble sort
+		for (int i = 0; i < ints.length - 1; i++) {
+			if (ints[i] > ints[i + 1]) {
+				int temp = ints[i];
+				ints[i] = ints[i + 1];
+				ints[i + 1] = temp;
+			}
+		}
+
+		return ints;
+	}
+
 	public void printArray(int[] ints) {
 		for (int i = 0; i < ints.length; i++) {
 			System.out.print(new Integer(ints[i]) + " ");
@@ -53,6 +95,11 @@ public class Sorts {
 		System.out.println("Bubble Sort");
 		int[] bsNumbers = bs.bubblesort(numbers);
 		bs.printArray(bsNumbers);
+		System.out.println();
+		
+		System.out.println("Recursive Bubble Sort");
+		int[] recursiveBsNumbers = bs.recursiveBubble(numbers);
+		bs.printArray(recursiveBsNumbers);
 		System.out.println();
 	}
 
