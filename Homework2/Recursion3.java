cd974fc27c21d5a74ed31639579c4c2fb0536a2d
Hobo Noir <hobonetics@gmail.com>
2017-03-05 21:27:53 -0500

    recursion3 problem added

---
+++ Homework2/Recursion3.java
@@ -0,0 +1,70 @@
+package Homework2;
+
+import java.util.ArrayList;
+
+public class Recursion3 {
+
+	public boolean isPalindrome(String str) {
+
+		str = str.toLowerCase();
+		
+		int begin = 0;
+		int end = str.length() - 1;
+		int length = str.length();
+
+		// strip punctuation/whitespace at beginning
+		while (str.charAt(begin) < 'a' || str.charAt(end) > 'z') {
+			begin++;
+			length--;
+			// hit base-case
+			if (begin == end - 1) {
+				return true;
+			}
+		}
+
+		// strip punctuation/whitespace at end
+		while (str.charAt(end) < 'a' || str.charAt(end) > 'z') {
+			end--;
+			length--;
+			// hit base-case
+			if (begin == end - 1) {
+				return true;
+			}
+		}
+
+		// base case
+		if (str.length() < 2) {
+			return true;
+		}
+
+		// check first and last string && middle
+		String substring = str.substring(begin+1, end);
+		if (str.charAt(begin) == str.charAt(end) && isPalindrome(substring)) {
+			// check the middle
+			return true;
+		}
+
+		return false;
+	}
+
+	public static void main(String[] args) {
+
+		ArrayList<String> strs = new ArrayList<String>();
+		strs.add("A but tuba.");
+		strs.add("A car, a man, a maraca.");
+		strs.add("Aibohphobia (fear of palindromes)");
+		
+		strs.add("Ummmmm that aren't");
+		strs.add("Just any word");
+		strs.add("Mirror");
+		
+		Recursion3 rc = new Recursion3();
+		
+		for(String str: strs){
+			System.out.println(str);
+			System.out.println(rc.isPalindrome(str));
+		}
+
+	}
+
+}
---
+++ Homework2/Testing.java
@@ -0,0 +1,16 @@
+package Homework2;
+
+public class Testing {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println('a' == 'a');
+		System.out.println('a' < 'b');
+		System.out.println('a' > 'b');
+		
+		
+
+	}
+
+}
