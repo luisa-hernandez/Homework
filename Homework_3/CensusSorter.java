f3ebeaa57e79afc47a6f3b6a3f58eeb7424b28c7
Luisa Hernandez <luisa@edu.chat>
2017-04-02 17:14:07 -0400

    made stub

--- Homework_3/CensusSorter.java
+++ Homework_3/CensusSorter.java
@@ -1,9 +1,19 @@
 package Homework_3;
 
+import java.util.ArrayList;
+
 public class CensusSorter {
 
+	public ArrayList readFile(){
+		return new ArrayList();
+	}
+	
+	public void sortData(int startYear, int endYear, String algo){
+		
+	}
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//read data file
+		
 
 	}
 
