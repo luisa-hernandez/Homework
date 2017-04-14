8b3f11ae22c7ebca8d816321f65455a7e0a15896
Hobo Noir <hobonetics@gmail.com>
2017-04-14 16:58:45 -0400

    customer now has wait time

--- hw4/Customer.java
+++ hw4/Customer.java
@@ -1,6 +1,33 @@
 package hw4;
 
 public class Customer {
+	/*
+	 * store customer arrival time & id
+	 */
 
+	private int id;
+	private int arrivalTime;
+	private int waitTime;
+
+	public Customer(int id, int arrivalTime) {
+		this.id = id;
+		this.arrivalTime = arrivalTime;
+	}
+
+	public int id() {
+		return id;
+	}
+
+	public int arrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setWaitTime(int waitTime) {
+		this.waitTime = waitTime;
+	}
+	
+	public int waitTime(){
+		return waitTime;
+	}
 	
 }
--- hw4/Simulation.java
+++ hw4/Simulation.java
@@ -1,6 +1,13 @@
 package hw4;
 
 public class Simulation {
+	/*
+	 *Program to simulate a business serving customers
+	 * 
+	 */
+	
+	//class variables -- describe program state
+	
 	
 	
 	public static void main(String[] args){
