e591d632f88f40238bf198cfb53e09b852cf93b5
Hobo Noir <hobonetics@gmail.com>
2017-04-14 17:13:23 -0400

    rought draft done

--- hw4/Simulation.java
+++ hw4/Simulation.java
@@ -1,5 +1,7 @@
 package hw4;
 
+import java.util.ArrayList;
+
 public class Simulation {
 	/*
 	 *Program to simulate a business serving customers
@@ -7,11 +9,35 @@ public class Simulation {
 	 */
 	
 	//class variables -- describe program state
+	private ArrayList<Customer> customers;
+	private int longestBreak;
+	private int idleTime;
+	private int longestLineGot;
+	private ArrayList<String> queries;
 	
+	public Simulation(String customerFilePath, String queryFilePath){
+		//read data from files
+		methodThatDoesThis();
+		//process data and queries
+		methodThatDoesThat();
+		
+		//maybe print results here?
+	}
 	
+	private void methodThatDoesThis(){
+		
+	}
+	
+	private void methodThatDoesThat(){
+		
+	}
+	
+	//method that prints results
 	
 	public static void main(String[] args){
-		
+		//args[0] = customer file path
+		//args[1] = query file path
+		Simulation sim = new Simulation(args[0], args[1]);
 	}
 
 }
