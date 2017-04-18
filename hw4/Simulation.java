240550018ecaedb559023bf25e9461c66b5ea7ff
Luisa Hernandez <luisa@edu.chat>
2017-04-18 18:35:17 -0400

    homework

--- hw4/Simulation.java
+++ hw4/Simulation.java
@@ -7,6 +7,20 @@ public class Simulation {
 	 * Program to simulate a business serving customers
 	 * 
 	 */
+	
+	//the queues
+	customerQueue<Customer>[] queues = new customerQueue[customers];
+	queriesQueue<String>[] 	queues = new queriesQueue[queries];
+	
+	//instantiate the queues
+	for (int i = 0; i < customers; i++) {
+		queues[i] = new customerQueue<Customer>();
+	}
+	
+	for (int i = 0; i < queries; i++) {
+		queues[i] = new queriesQueue<String>();
+	}
+	
 
 	// class variables -- describe program state
 	private ArrayList<Customer> customers;
@@ -15,6 +29,19 @@ public class Simulation {
 	private int longestBreak;
 	private int idleTime;
 	private int longestLineGot;
+	
+	//next customer from generator
+	Customer nextCust; 
+	
+	//holds customer for temporary use
+	Customer cust; 
+	
+	//count of customers started so far
+	int custStartCount = 0;
+	
+	//count of customers finished so far
+	int custEndCount = 0;
+	
 
 	public Simulation(String customerFilePath, String queryFilePath) {
 		// read data from files
@@ -30,6 +57,7 @@ public class Simulation {
 
 	private void readCustomerData(String customerFilePath) {
 		// update "customer" class variable
+		
 
 		// create a customer object for every customer
 	}
@@ -41,16 +69,24 @@ public class Simulation {
 	private void processData() {
 		// update longest break, total idle time, and max line length
 		// "I guess just update all the counters."
+		
+		//set customer generator and get first customer
+		customer.Gen.reset();
+		nextCust = customer.Gen.nextCustomer();
+		
 	}
 
 	// method that prints results
 	private void outputData() {
 		// make them fancy print statements
+		
 	}
 
 	public static void main(String[] args) {
 		// args[0] = customer file path
 		// args[1] = query file path
+		
+		//create object to perform simulation
 		Simulation sim = new Simulation(args[0], args[1]);
 	}
 
