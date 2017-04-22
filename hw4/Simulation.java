33dc5b04eb3a961ff4aeab07e273a69d399136a0
Luisa Hernandez <luisa@edu.chat>
2017-04-21 23:41:58 -0400

    processing customers

--- hw4/Simulation.java
+++ hw4/Simulation.java
@@ -9,7 +9,6 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-
 import Homework_1.Student;
 
 public class Simulation<G> {
@@ -27,36 +26,39 @@ public class Simulation<G> {
 	private int idleTime;
 	private int longestLineGot;
 	private int serviceTime;
+	private int customersServed;
+	private ArrayList<String> queries;
 
 	public Simulation(String customerFilePath, String queryFilePath) {
+		customersServed = 0;
+		longestLineGot =0;
+		idleTime = 0;
 		// read data from files
 		readCustomerData(customerFilePath);
 		readQueryData(queryFilePath);
 
 		// process data and queries
-		processData();
+		processCustomers();
 
 		// maybe print results here?
 		outputData();
 	}
-	
-	private int convertToSeconds (String arrivalTime){
+
+	private int convertToSeconds(String arrivalTime) {
 		String[] arrivalTimeArray = arrivalTime.split(":");
 		int hours = Integer.parseInt(arrivalTimeArray[0]);
-		if (hours >=1 && hours <=5){
+		if (hours >= 1 && hours <= 5) {
 			hours += 12;
 		}
 		int minutes = Integer.parseInt(arrivalTimeArray[1]);
 		int seconds = Integer.parseInt(arrivalTimeArray[2]);
-		return hours*60*60 + minutes*60 + seconds;
+		return hours * 60 * 60 + minutes * 60 + seconds;
 	}
-	
 
 	private void readCustomerData(String customerFilePath) {
 		/**
 		 * load customers from a file
 		 */
-		File customerFile = new File(customerFilePath);
 
 		try (BufferedReader reader = new BufferedReader(new FileReader(customerFilePath))) {
 			String line = reader.readLine();
@@ -67,34 +69,36 @@ public class Simulation<G> {
 			// read the customer data
 			line = reader.readLine();
 			while (line != null) {
-				//read line to get id
+				// read line to get id
 				line = reader.readLine();
-				//get integer from end of string
+				// get integer from end of string
 				int lastSpace = line.lastIndexOf(" ");
-				String idString = line.substring(lastSpace+1);
+				String idString = line.substring(lastSpace + 1);
 				int id = Integer.parseInt(idString);
-				
-				//read line to get arrival time
+
+				// read line to get arrival time
 				line = reader.readLine();
-				//get arrival time from end of string
+				// get arrival time from end of string
 				lastSpace = line.lastIndexOf(" ");
-				String arrivalTimeString = line.substring(lastSpace+1);
+				String arrivalTimeString = line.substring(lastSpace + 1);
 				int arrivalTime = convertToSeconds(arrivalTimeString);
-				
-				//create customer object, attach to queue
+
+				// create customer object, attach to queue
 				Customer c = new Customer(id, arrivalTime);
+
+				//TODO: only if customer arrived before 5PM
+				//if statement
 				
-				//attach customer to last customer
-				if(firstCustomer == null){
+				// attach customer to last customer
+				if (firstCustomer == null) {
 					firstCustomer = c;
 					lastCustomer = c;
-				}
-				else{
+				} else {
 					lastCustomer.setNextCustomer(c);
 					lastCustomer = c;
 				}
-				
-				//lastly
+
+				// lastly
 				line = reader.readLine();
 			}
 
@@ -107,21 +111,61 @@ public class Simulation<G> {
 
 	private void readQueryData(String queryFilePath) {
 		// update "queries" class variable
+		queries = new ArrayList<String>();
+
+		try (BufferedReader reader = new BufferedReader(new FileReader(queryFilePath))) {
+			String line = reader.readLine();
+			while (line != null && line != ""){
+				queries.add(line);
+			}
+
+		}
+
+		catch (IOException e) {
+			// first time running
+		}
 	}
 
-	private void processData() {
+	private void processCustomers() {
 		// update longest break, total idle time, and max line length
 		// "I guess just update all the counters."
-	}
-
+		Customer currentCustomer = firstCustomer;
+		int currentTime = 9*60*60;
+		int breakTime = 0;
+		while(currentCustomer != null){
+			if(currentCustomer.arrivalTime() > currentTime){
+				breakTime = currentCustomer.arrivalTime() - currentTime;
+				currentTime = currentCustomer.arrivalTime();
+			}
+			else{
+				currentTime += serviceTime;
+				breakTime = 0;
+			}
+			customersServed += 1;
+			currentCustomer = currentCustomer.nextCustomer();
+			
+			Customer lineCustomer = currentCustomer;
+			int lineLength = 0;
+			
+			while (lineCustomer != null && lineCustomer.arrivalTime() <= currentTime){
+				lineLength +=1;
+				lineCustomer = lineCustomer.nextCustomer();
+			}
+			if(lineLength > longestLineGot){
+				longestLineGot = lineLength;
+			}
+			
+		}
+	}		
+	
 	// method that prints results
 	private void outputData() {
 		// make them fancy print statements
-		System.out.println("The person will wait:" );
+		System.out.println("The person will wait:");
 		System.out.println("Cutomers in the group served: ");
 		System.out.println("Total idle time of employee: ");
 		System.out.println("Longest break employee has: ");
-		System.out.println("Longest waiting line: " );
+		System.out.println("Longest waiting line: ");
 
 	}
 
