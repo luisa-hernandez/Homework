177bb9b05661bbe33bd8454c7bf4163a719e3df6
Luisa Hernandez <luisa@edu.chat>
2017-05-02 22:18:23 -0400

    begin to read files

--- hw5/Community.java
+++ hw5/Community.java
@@ -8,19 +8,27 @@ public class Community {
 
 	public Community(String peopleFile, String queryFile) {
 		// TODO 1 -read input files and process information
-
 		// query file
-
+		readQueryFile(queryFile);
+		
 		// person files
+		readPersonFile(peopleFile);
 		
-		//read and put into temporary array list
+		// read and put into temporary array list
 		
-		
-		//shuffle the array list using java.util.Collections.shuffle
-		
-		
-		// put it into a binary search tree
+		// shuffle the array list using java.util.Collections.shuffle
 
+		// put it into a binary search tree
+	}
+
+	
+	private void readQueryFile(String queryFilePath) {
+		queries = new ArrayList<String>();
+		//TODO: do the rest of the things
+	}
+	
+	private void readPersonFile(String personFilePath){
+		ArrayList people = new ArrayList<Person>();
 	}
 
 	public static void main(String[] args) {
