649eba0ddd60a0fed2c691c6bfac19d6de1e3655
Hobo Noir <hobonetics@gmail.com>
2017-05-02 21:59:20 -0400

    added teh codez

--- hw5/Community.java
+++ hw5/Community.java
@@ -1,8 +1,30 @@
 package hw5;
 
+import java.util.ArrayList;
+
 public class Community {
-	public static void main(String[] args){
+
+	private ArrayList<String> queries;
+
+	public Community(String peopleFile, String queryFile) {
+		// TODO 1 -read input files and process information
+
+		// query file
+
+		// person files
 		
+		//read and put into temporary array list
+		
+		
+		//shuffle the array list using java.util.Collections.shuffle
+		
+		
+		// put it into a binary search tree
+
+	}
+
+	public static void main(String[] args) {
+		// TODO 0 - instantiate Community class w/ paths of files
 	}
 
 }
