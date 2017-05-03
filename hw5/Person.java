27c84ab39e61f5378ba78102c1f065c0319c6aa3
Hobo Noir <hobonetics@gmail.com>
2017-05-02 21:38:12 -0400

    updated person class

--- hw5/Person.java
+++ hw5/Person.java
@@ -1,29 +1,51 @@
 package hw5;
 
+import java.util.ArrayList;
+
 public class Person {
-	
-	//variable members
+
+	// variable members
 	private int SSN;
 	private String firstName;
 	private String lastName;
 	private int motherSSN;
 	private int fatherSSN;
-	//friends
-	
-	//constructor should take as input the data pieces provided in the input file
-	public Customer(String firstName, String lastName, int SSN, int motherSSN, int fatherSSN //, friends) {
+	ArrayList<Integer> friends;
+
+	// constructor should take as input the data pieces provided in the input
+	// file
+	public Person(String firstName, String lastName, int SSN, int motherSSN, int fatherSSN,
+			ArrayList<Integer> friends) {
 		this.firstName = firstName;
 		this.lastName = lastName;
 		this.SSN = SSN;
 		this.motherSSN = motherSSN;
 		this.fatherSSN = fatherSSN;
-		//friends
+		this.friends = friends;
 	}
-	//accessors to return the values:
-	//getSSN()
-	//getFullName()
-	//getFatherSSN()
-	//getMotherSSN()
-	//getFriends()
+
+	public int getSSN() {
+		return SSN;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public int getMotherSSN() {
+		return motherSSN;
+	}
+
+	public int getFatherSSN() {
+		return fatherSSN;
+	}
+
+	public ArrayList<Integer> getFriends() {
+		return friends;
+	}
 
 }
