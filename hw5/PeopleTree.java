02a49110e0e0b85df6758622aa0bbe8181169a4a
Luisa Hernandez <luisa@edu.chat>
2017-05-05 17:22:20 -0400

    attempted find and delete

--- hw5/PeopleTree.java
+++ hw5/PeopleTree.java
@@ -8,12 +8,33 @@ public class PeopleTree<K extends Comparable<K>, V> {
 	}
 
 	public V find(K key) {
-		return null;
+		if (root == null){
+			return null;
+		}
+		if (node.compareTo(current) < 0) {
+			return find(node, root.left);
+		}
+		else if (node.compareTo(current) > 0){
+			return find(node, root.right);
+		}
+		else{
+			//this means it's a match
+			return root;
+		}
 	}
 
-//	public void delete(K key) {
-//
-//	}
+	public void delete(K key) {
+		
+		
+		if (node.compareTo(current) < 0){
+			node.left = remove (key);
+		}
+		else if (node.compareTo(current) > 0){
+			node.right = remove(key);
+		}
+		else if
+
+	}
 
 	public void insert(K key, V value) {
 		PNode<K, V> node = new PNode<K, V>(key, value);
@@ -32,17 +53,28 @@ public class PeopleTree<K extends Comparable<K>, V> {
 						//left is free, so set left child
 						current.setLeft(node);
 						done = true;
-					}else{
+					}
+					
+					else{
 						//left is not free, set current to left
 						current = current.getLeft();
 					}
 
 				} else {
 					// go to right
+					if(current.getRight()==null){
+					
+					}
+					else{
+						current = current.getRight();
+					}
 					
 					//TODO -- write code to insert into right child
 					
 					// 1- is right free?
+					current.setRight(node);
+					done = true;
+					current = current.getRight();
 					
 					// 2- if not, compare to right child
 					
