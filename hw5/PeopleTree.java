9fa4dd8f7879b8534bda0f1290fd7e502da54493
Hobo Noir <hobonetics@gmail.com>
2017-05-05 17:52:41 -0400

    updated finding PNode in tree

--- hw5/PeopleTree.java
+++ hw5/PeopleTree.java
@@ -2,25 +2,33 @@ package hw5;
 
 public class PeopleTree<K extends Comparable<K>, V> {
 
-	PNode<K, V> root = null;
+	PNode<K, V> root;
 
 	public PeopleTree() {
 	}
 
-	public V find(K key) {
-		if (root == null){
-			return null;
+	/**
+	 * Finding a PNode based on key. Return null if not found.
+	 */
+	public PNode<K, V> find(K key) {
+		PNode<K, V> node = root;
+		boolean notFound = true;
+
+		while (notFound) {
+			if (node == null) {
+				notFound = false;
+			} else if (key.compareTo(node.getKey()) == 0) {
+				// match
+				notFound = false;
+			} else if (key.compareTo(node.getKey()) < 0) {
+				// look at left child
+				node = node.getLeft();
+			} else {
+				// look at right child
+				node = node.getRight();
+			}
 		}
-		if (node.compareTo(current) < 0) {
-			return find(node, root.left);
-		}
-		else if (node.compareTo(current) > 0){
-			return find(node, root.right);
-		}
-		else{
-			//this means it's a match
-			return root;
-		}
+		return node;
 	}
 
 	public void delete(K key) {
@@ -46,39 +54,37 @@ public class PeopleTree<K extends Comparable<K>, V> {
 
 			while (!done) {
 				// compare keys
-				
+
 				if (node.compareTo(current) < 0) {
 					// go to left
-					if(current.getLeft() == null){
-						//left is free, so set left child
+					if (current.getLeft() == null) {
+						// left is free, so set left child
 						current.setLeft(node);
 						done = true;
 					}
-					
-					else{
-						//left is not free, set current to left
+
+					else {
+						// left is not free, set current to left
 						current = current.getLeft();
 					}
 
 				} else {
 					// go to right
-					if(current.getRight()==null){
-					
-					}
-					else{
+					if (current.getRight() == null) {
+
+					} else {
 						current = current.getRight();
 					}
-					
-					//TODO -- write code to insert into right child
-					
+
+					// TODO -- write code to insert into right child
+
 					// 1- is right free?
 					current.setRight(node);
 					done = true;
 					current = current.getRight();
-					
+
 					// 2- if not, compare to right child
-					
-					
+
 				}
 
 			}
