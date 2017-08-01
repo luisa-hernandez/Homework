d0e177323f897142d8203ddc207b591dad9ba47c
Hobo Noir <hobonetics@gmail.com>
2017-08-01 14:49:20 -0400

    Sentiment reads comments

--- pa_project/Sentiment.java
+++ pa_project/Sentiment.java
@@ -9,107 +9,162 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-public class Sentiment {
+public class Sentiment2 {
 
 	// create hashmap to hold source objects
 	private HashMap<String, Source> sources;
-
 	private ArrayList<String> sourceNames;
+	private ArrayList<String> topics;
 
-	public Sentiment(String commentData) {
+	// private ArrayList<String> sourceNames;
+
+	public Sentiment2(String commentData) {
 		// 1: create sources hashmap
 		sources = new HashMap<String, Source>();
 
-		// insert each source object with a blank arraylist
-		ArrayList<String> sourceNames = new ArrayList<String>();
+		// source names
+		sourceNames = new ArrayList<String>();
+		sourceNames.add("MSNBC");
+		sourceNames.add("youngTurks");
+		sourceNames.add("BBC");
+		sourceNames.add("CNN");
+		sourceNames.add("deFranco");
+		sourceNames.add("Fox");
+		sourceNames.add("Jones");
 
-//		// insert source names into array list
-//		sourceNames.add("MSNBC");
-//		sourceNames.add("youngTurks");
-//		sourceNames.add("BBC");
-//		sourceNames.add("CNN");
-//		sourceNames.add("deFranco");
-//		sourceNames.add("Fox");
-//		sourceNames.add("Jones");
-
-		// use a loop on this array list to add each source to our source list
-		for (String name : sourceNames) {
-			Source s = new Source();
-			sources.put(name, s);
+		// create sources
+		for (String sourceName : sourceNames) {
+			sources.put(sourceName, new Source());
 		}
+
+		// topic names
+		topics = new ArrayList<String>();
+		topics.add("testimony");
+		topics.add("fired");
+		topics.add("election");
+		topics.add("agreement");
+		topics.add("m_attack");
+		topics.add("l_attack");
+		topics.add("ban");
+		topics.add("pope");
+		topics.add("baseball");
+
+		// add topics to sources
+		for (String sourceName : sourceNames) {
+			Source source = sources.get(sourceName);
+			for (String topic : topics) {
+				source.addTopic(topic);
+			}
+		}
+
 		System.out.println();
 	}
 
-	private void readFile(String filePath) throws FileNotFoundException, IOException {
+	public void readFile(String filePath) throws FileNotFoundException, IOException {
+
 		try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
 
 			// read first line
 			String line = reader.readLine();
 			line = reader.readLine();
 
-			sourceNames = new ArrayList<String>();
-
-			// insert source names into array list
-			sourceNames.add("MSNBC");
-			sourceNames.add("youngTurks");
-			sourceNames.add("BBC");
-			sourceNames.add("CNN");
-			sourceNames.add("deFranco");
-			sourceNames.add("Fox");
-			sourceNames.add("Jones");
-
-			ArrayList<String> topics = new ArrayList<String>();
-			topics.add("testimony");
-			topics.add("fired");
-			topics.add("election");
-			topics.add("agreement");
-			topics.add("m_attack");
-			topics.add("l_attack");
-			topics.add("ban");
-			topics.add("pope");
-			topics.add("baseball");
-
-			// add topics to sources
-			for (Source source : sources.values()) {
-				for (String topic : topics) {
-					source.addTopic(topic);
-				}
-			}
-
-			// read comments and insert into data source object
-
-			// step 1: determine size of data grid
-			ArrayList<String> comments;
-			// step 2: create array list of source names to maintain order
-
 			while (line != null && line != "") {
-				comments = new ArrayList<String>();
+				// step 1: parse line
+				ArrayList<String> comments = new ArrayList();
 				comments.addAll(Arrays.asList(line.split("\t")));
-				// step 3: check for correct amount of values
-				// step 4: insert null in extra spaces
-				while (comments.size() < topics.size() * sources.size()) {
-					comments.add(null);
+
+				// hard coded :(
+				while (comments.size() < 63) {
+					comments.add("");
 				}
 
-				for (String sourceName : sourceNames) {
-					for (String topic : topics) {
-						sources.get(sourceName).addComment(topic, comments.remove(0));
-
+				// add comments to sources
+				for (String topicName : topics) {
+					for (String sourceName : sourceNames) {
+						sources.get(sourceName).addComment(topicName, comments.remove(0));
 					}
 				}
+
 				line = reader.readLine();
+
 			}
 			System.out.println();
+
 		}
-		System.out.println();
+
 	}
 
-	public static void main(String[] args) throws FileNotFoundException, IOException {
-		Sentiment internetComments = new Sentiment(args[0]);
-		internetComments.readFile(args[0]);
-		// Source CNN = internetComments.sources.get("CNN");
-		System.out.println("");
+	// private void readFile(String filePath) throws FileNotFoundException,
+	// IOException {
+	// try (BufferedReader reader = new BufferedReader(new
+	// FileReader(filePath))) {
+	//
+	// // read first line
+	// String line = reader.readLine();
+	// line = reader.readLine();
+	//
+	// sourceNames = new ArrayList<String>();
+	//
+	// // insert source names into array list
+	// sourceNames.add("MSNBC");
+	// sourceNames.add("youngTurks");
+	// sourceNames.add("BBC");
+	// sourceNames.add("CNN");
+	// sourceNames.add("deFranco");
+	// sourceNames.add("Fox");
+	// sourceNames.add("Jones");
+	//
+	// ArrayList<String> topics = new ArrayList<String>();
+	// topics.add("testimony");
+	// topics.add("fired");
+	// topics.add("election");
+	// topics.add("agreement");
+	// topics.add("m_attack");
+	// topics.add("l_attack");
+	// topics.add("ban");
+	// topics.add("pope");
+	// topics.add("baseball");
+	//
+	// // add topics to sources
+	// for (Source source : sources.values()) {
+	// for (String topic : topics) {
+	// source.addTopic(topic);
+	// }
+	// }
+	//
+	// // read comments and insert into data source object
+	//
+	// // step 1: determine size of data grid
+	// ArrayList<String> comments;
+	// // step 2: create array list of source names to maintain order
+	//
+	// while (line != null && line != "") {
+	// comments = new ArrayList<String>();
+	// comments.addAll(Arrays.asList(line.split("\t")));
+	// // step 3: check for correct amount of values
+	// // step 4: insert null in extra spaces
+	// while (comments.size() < topics.size() * sources.size()) {
+	// comments.add(null);
+	// }
+	//
+	// for (String sourceName : sourceNames) {
+	// for (String topic : topics) {
+	// sources.get(sourceName).addComment(topic, comments.remove(0));
+	//
+	// }
+	// }
+	// line = reader.readLine();
+	// }
+	// System.out.println();
+	// }
+	// System.out.println();
+	// }
 
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		Sentiment2 internetComments = new Sentiment2(args[0]);
+		internetComments.readFile(args[0]);
+
+		System.out.println("");
 	}
 
 }
